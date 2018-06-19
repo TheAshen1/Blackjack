64eb4e5fa1bc082e9fb3835291145a4838a63bd4
TheAshen1 <theexaltedmax@gmail.com>
2018-06-19 13:39:59 +0300

    little tweak with namespaces

--- DataAccess/ConnectionFactory.cs
+++ DataAccess/ConnectionFactory.cs
@@ -16,7 +16,7 @@ namespace DataAccess
         public DbConnection CreateConnection(string nameOrConnectionString)
         {
             //string ConnectionString = ConfigurationManager.ConnectionStrings[nameOrConnectionString];
-            string ConnectionString = Properties.Resources.ResourceManager.GetString(nameOrConnectionString);
+            string ConnectionString = BlackJack.DataAccess.Properties.Resources.ResourceManager.GetString(nameOrConnectionString);
             var factory = DbProviderFactories.GetFactory("System.Data.SqlClient");
             var conn = factory.CreateConnection();
             conn.ConnectionString = ConnectionString;
--- DataAccess/Properties/Resources.Designer.cs
+++ DataAccess/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DataAccess.Properties {
+namespace BlackJack.DataAccess.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DataAccess.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DataAccess.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BlackJack.DataAccess.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
